1e9491b8765a9632845ffd0d62b0898186038533
RealGolby <golbygames@gmail.com>
2022-02-05 22:55:32 +0100

    test

--- Assets/Scripts/MainMenu/NetworkPlayer.cs
+++ Assets/Scripts/MainMenu/NetworkPlayer.cs
@@ -3,5 +3,13 @@ using Mirror;
 
 public class NetworkPlayer : NetworkBehaviour
 {
+    public string Name;
 
+    NicknameManager nicknameManager;
+    private void Start()
+    {
+        nicknameManager = FindObjectOfType<NicknameManager>();
+        Name = nicknameManager.GetNickName();
+        Debug.Log("my epic nicknam");
+    }
 }
