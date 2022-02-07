2beb8149d8bca3b4332c47a810c2f581458cd916
VASABIcz <vasabisroa@gmail.com>
2022-02-08 00:11:11 +0100

    Add chonky ass boa for calculating penetration !IDK if it works

--- Assets/Scripts/player/PlayerGunManager.cs
+++ Assets/Scripts/player/PlayerGunManager.cs
@@ -1,13 +1,30 @@
+using System.Linq;
+using JetBrains.Annotations;
+using material;
+using Mirror;
 using UnityEngine;
 
-public class PlayerGunManager : MonoBehaviour
+public class PlayerGunManager : NetworkBehaviour
 {
+    public Collider coll;
+
     public Gun Primary;
     public Gun Secondary;
+    private Camera camyr;
+
+    private void Start()
+    {
+        if (!isLocalPlayer) return;
+
+        camyr = Camera.main;
+        coll = GetComponent<Collider>();
+    }
 
     private void Update()
     {
-        constoll();
+        if (!isLocalPlayer) return;
+
+        control();
     }
 
     public void Shoot()
@@ -20,7 +37,8 @@ public class PlayerGunManager : MonoBehaviour
         // todo
     }
 
-    public void constoll()
+
+    public void control()
     {
         // todo
         // switch weapons
@@ -28,5 +46,70 @@ public class PlayerGunManager : MonoBehaviour
         // reload
         // drop weapon
         // pickup weapon
+    }
+
+    // Debug.DrawRay(pos.position, pos.TransformDirection(Vector3.forward) * 10, Color.cyan);
+    public BulletPath BulletPenetration(Transform pos, int maxpen = 5) // FIXME
+    {
+        var dierection = pos.TransformDirection(Vector3.forward) * 10;
+
+        BulletImpact[] impacts = null;
+        RaycastHit? hitus = null;
+        var damagemod = 0f;
+
+        var origin = pos;
+
+        for (var i = 0; i <= maxpen; i++)
+        {
+            var ray = new Ray(origin.position, dierection);
+
+            if (Physics.Raycast(ray, out var hit) && hit.collider.GetComponent<EntityBase>())
+            {
+                hitus = hit;
+            }
+            else if (hit.collider.GetComponent<Penetration>())
+            {
+                var pen = hit.collider.GetComponent<Penetration>();
+
+                if (coll.Raycast(new Ray(hit.transform.position + Vector3.one * 20, dierection * -1), out var hitb,
+                        50.0f))
+                {
+                    var len = (hitb.transform.position - hit.transform.position).magnitude;
+                    origin = hitb.transform; // todo some small offset
+
+                    damagemod += pen.value * len;
+                    if (damagemod >= 100)
+                    {
+                        impacts.Append(new BulletImpact {Location = hit, Penetrated = true});
+                        impacts.Append(new BulletImpact {Location = hitb, Penetrated = false});
+                        return new BulletPath {Impacts = impacts, hit = hitus, DamageModifier = damagemod};
+                    }
+
+                    impacts.Append(new BulletImpact {Location = hit, Penetrated = true});
+                    impacts.Append(new BulletImpact {Location = hitb, Penetrated = true});
+                }
+                else
+                {
+                    impacts.Append(new BulletImpact {Location = hit, Penetrated = false});
+                    return new BulletPath {Impacts = impacts, hit = hitus, DamageModifier = damagemod};
+                }
+            }
+        }
+
+        return new BulletPath {Impacts = impacts, hit = hitus, DamageModifier = damagemod};
+    }
+
+
+    public struct BulletImpact
+    {
+        public bool Penetrated;
+        public RaycastHit Location;
+    }
+
+    public struct BulletPath
+    {
+        [CanBeNull] public BulletImpact[] Impacts;
+        public RaycastHit? hit;
+        public float? DamageModifier;
     }
 }
