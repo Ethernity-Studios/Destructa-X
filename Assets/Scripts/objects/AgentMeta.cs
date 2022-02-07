82b37d925d39b88446c6d9a9dc686be4ae8e77ef
VASABIcz <vasabisroa@gmail.com>
2022-02-08 00:44:37 +0100

    AgentBase classes

---
+++ Assets/Scripts/objects/AgentMeta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace objects
+{
+    public enum AgentClass
+    {
+        Sentine,
+        Duelist
+    }
+
+    [Serializable]
+    public class AgentMeta
+    {
+        public GameObject Object;
+        public Sprite Icon;
+        public AgentClass AClass;
+    }
+}
