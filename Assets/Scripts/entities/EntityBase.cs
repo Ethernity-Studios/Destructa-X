c98e9bf61e32679ccd8633091f9930f71d27b9f3
VASABIcz <vasabisroa@gmail.com>
2022-02-08 00:09:44 +0100

    added OwOner for entity

--- Assets/Scripts/entities/EntityBase.cs
+++ Assets/Scripts/entities/EntityBase.cs
@@ -1,13 +1,21 @@
 using Mirror;
 
+public enum OwOner
+{
+    Blue,
+    Red,
+    Neutral
+}
+
 public abstract class EntityBase : NetworkBehaviour
 {
     public float Health;
     public float MaxHealth;
     public bool Dead;
+    public OwOner Type;
 
 
     public abstract bool TakeDamage(float damage);
 
-    public abstract void Heal(float damage);
+    public abstract void Heal(float hp);
 }
