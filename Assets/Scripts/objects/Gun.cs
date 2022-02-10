fa9a308488b79eee0ccbf1c38b9e05f9e4c459bc
VASABIcz <vasabisroa@gmail.com>
2022-02-10 22:39:03 +0100

    usefull commit message

--- Assets/Scripts/objects/Gun.cs
+++ Assets/Scripts/objects/Gun.cs
@@ -7,6 +7,10 @@ public class Gun : ScriptableObject
     public int MagazineSize;
     public int MaxAmmo;
     public int ReloadTime;
+    public GunType Type;
+
+    public GameObject GunModel;
+    public Sprite GunIcon;
 
     public Fire LMB;
     public RMB RMB;
