6609e15290021db7748c6200f6de77106a51786a
RealGolby <73080347+RealGolby@users.noreply.github.com>
2022-03-25 10:26:13 +0100

    anti explosionen :girl:

--- Assets/Scripts/Game/Explosion.cs
+++ Assets/Scripts/Game/Explosion.cs
@@ -3,15 +3,33 @@ using UnityEngine;
 public class Explosion : MonoBehaviour
 {
     BombManager bombManager;
+
+    bool exploded = false;
+
+    bool decrease = false;
     private void Start()
     {
         bombManager = FindObjectOfType<BombManager>();
     }
     private void Update()
     {
-        if (transform.localScale.x < bombManager.ExplosionSize)
+        if (transform.localScale.x < bombManager.ExplosionSize && !exploded)
         {
             transform.localScale += new Vector3(bombManager.IncreaseSize * Time.deltaTime, bombManager.IncreaseSize * Time.deltaTime /2, bombManager.IncreaseSize * Time.deltaTime);
         }
+
+        if(transform.localScale.x >= bombManager.ExplosionSize)
+        {
+            exploded = true;
+            Invoke("canDecrease", 5f);
+        }
+        if (transform.localScale.x > 0 && decrease)
+        {
+            transform.localScale -= new Vector3(bombManager.IncreaseSize * Time.deltaTime * 2, bombManager.IncreaseSize * Time.deltaTime, bombManager.IncreaseSize * Time.deltaTime * 2);
+        }
+    }
+    void canDecrease()
+    {
+        decrease = true;
     }
 }
