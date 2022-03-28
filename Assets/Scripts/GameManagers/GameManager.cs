116dc27afddd245e30093c3ea9d6fdbb9b866d57
RealGolby <golbygames@gmail.com>
2022-03-28 23:36:24 +0200

    .

--- Assets/Scripts/GameManagers/GameManager.cs
+++ Assets/Scripts/GameManagers/GameManager.cs
@@ -53,6 +53,8 @@ public class GameManager : NetworkBehaviour
 
     public GameObject ShopUI;
 
+    [SerializeField] Transform[] blueSpawnPositions, redSpawnPositions;
+
     private void Start()
     {
         ShopUI.SetActive(false);
