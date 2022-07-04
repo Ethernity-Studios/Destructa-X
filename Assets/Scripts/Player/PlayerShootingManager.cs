b414900688683d739bbba19184929a2af729c5cf
RealGolby <golbygames@gmail.com>
2022-07-04 20:55:12 +0200

    Remade bullets into HitScans

--- Assets/Scripts/Player/PlayerShootingManager.cs
+++ Assets/Scripts/Player/PlayerShootingManager.cs
@@ -26,6 +26,7 @@ public class PlayerShootingManager : NetworkBehaviour
     }
     void Update()
     {
+        Debug.DrawRay(cameraHolder.position, cameraHolder.forward, Color.red);
         if (!isLocalPlayer) return;
         if (gunInstance == null) return;
 
@@ -57,15 +58,6 @@ public class PlayerShootingManager : NetworkBehaviour
         canShoot = true;
     }
 
-    public void Shoot()
-    {
-        canShoot = false;
-        StartCoroutine(DelayFire());
-        gunInstance.Magazine--;
-        CmdSpawnBullet();
-        UpdateUIAmmo();
-    }
-
     public IEnumerator Reload()
     {
         Reloading = true;
@@ -89,8 +81,100 @@ public class PlayerShootingManager : NetworkBehaviour
         uiManager.MaxAmmoText.text = gunInstance.Ammo.ToString();
         uiManager.MagazineText.text = gunInstance.Magazine.ToString();
     }
+    public void Shoot()
+    {
+        canShoot = false;
+        StartCoroutine(DelayFire());
+        gunInstance.Magazine--;
+        UpdateUIAmmo();
+        penetrationAmount = playerInventory.EqupiedGun.BulletPenetration;
+        CheckPenetration();
+    }
 
-    [Command]
+    [SerializeField] LayerMask mask;
+    int BulletDamage;
+    Vector3 endPoint;
+    Vector3? penetrationPoint;
+    Vector3? impactPoint;
+    bool canPenetrate;
+    float penetrationAmount;
+
+    public void CheckPenetration()
+    {
+        Debug.Log("Checking peentration");
+        Ray ray = new Ray(this.transform.position + new Vector3(0, 0, transform.localScale.z), this.transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(cameraHolder.position,cameraHolder.forward, out hit, Mathf.Infinity, mask))
+        {
+            if(hit.transform.parent.gameObject.TryGetComponent(out IDamageable entity))
+            {
+                Debug.Log("Dealing damage!");
+                entity.TakeDamage(CalculateDamage(hit.point));
+            }
+            Debug.Log(hit.transform.name + " hittd object");
+            impactPoint = hit.point;
+            Ray penRay = new Ray(hit.point + ray.direction * penetrationAmount, -ray.direction);
+            RaycastHit penHit;
+            if (hit.collider.Raycast(penRay, out penHit, penetrationAmount))
+            {
+                penetrationPoint = penHit.point;
+                endPoint = this.transform.position + this.transform.forward * 1000;
+
+                if (hit.transform.TryGetComponent(out MaterialToughness materialToughness))
+                {
+                    Debug.Log("1");
+                    penetrationAmount -= Vector3.Distance((Vector3)penetrationPoint, hit.point);
+                    penetrationAmount -= materialToughness.ToughnessAmount;
+                    CheckPenetration();
+                }
+                else
+                {
+                    Debug.Log("return");
+                    return;
+                }
+            }
+            else
+            {
+                Debug.Log("2");
+                endPoint = impactPoint.Value + ray.direction * penetrationAmount;
+                penetrationPoint = endPoint;
+                return;
+            }
+        }
+        else
+        {
+            Debug.Log("2");
+            endPoint = this.transform.position + this.transform.forward * 1000;
+            penetrationPoint = null;
+            impactPoint = null;
+        }
+    }
+
+
+    int CalculateDamage(Vector3 entityPosition)
+    {
+        Gun gun = playerInventory.EqupiedGun;
+        float distance = Vector3.Distance(entityPosition, cameraHolder.position);
+        Debug.Log("Distance: " + distance);
+        if (gun.Damages.Count == 1)
+        {
+            BulletDamage = gun.Damages[0].BodyDamage;
+        }
+        else if (gun.Damages.Count == 2)
+        {
+            if (distance <= gun.Damages[0].MaxDistance) BulletDamage = gun.Damages[0].BodyDamage;
+            else if (distance >= gun.Damages[1].MinDistance) BulletDamage = gun.Damages[1].BodyDamage;
+        }
+        else if (gun.Damages.Count == 3)
+        {
+            if (distance <= gun.Damages[0].MaxDistance) BulletDamage = gun.Damages[0].BodyDamage;
+            else if (distance >= gun.Damages[1].MinDistance && distance <= gun.Damages[1].MaxDistance) BulletDamage = gun.Damages[1].BodyDamage;
+            else if (distance >= gun.Damages[2].MinDistance) BulletDamage = gun.Damages[2].BodyDamage;
+        }
+        return BulletDamage;
+    }
+
+    /*[Command]
     void CmdSpawnBullet()
     {
         GameObject bulletInstance = Instantiate(bullet);
@@ -137,5 +221,5 @@ public class PlayerShootingManager : NetworkBehaviour
         bullet.BulletDirection = new Vector3(cameraHolder.eulerAngles.x, transform.eulerAngles.y);
         bullet.BulletOwner = GetComponent<Player>();
         bullet.Gun = playerInventory.EqupiedGun;
-    }
+    }*/
 }
