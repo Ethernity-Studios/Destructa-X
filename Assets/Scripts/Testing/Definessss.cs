9cc8eea788278ecb0184b5de34b2c4e766fc3806
RealGolby <golbygames@gmail.com>
2022-12-07 19:55:40 +0100

    :skull:

---
+++ Assets/Scripts/Testing/Definessss.cs
@@ -0,0 +1,17 @@
+#define VASAGEJ
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Definessss : MonoBehaviour
+{
+
+    private void Start()
+    {
+        System.Console.WriteLine("test");
+    }
+}
